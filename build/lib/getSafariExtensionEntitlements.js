27b11b1f8cc14c533d34e488e6715f76fbbc23dd
catalinmiron <mironcatalin@gmail.com>
2023-09-26 14:16:44 +0200

    Rebuild it

--- build/lib/getSafariExtensionEntitlements.js
+++ build/lib/getSafariExtensionEntitlements.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getSafariExtensionEntitlements = void 0;
 function getSafariExtensionEntitlements(iosConfig) {
-    var _a;
+    var _a, _b;
     const appBundleIdentifier = iosConfig === null || iosConfig === void 0 ? void 0 : iosConfig.bundleIdentifier;
     const entitlements = {
         "com.apple.developer.parent-application-identifiers": [
@@ -10,7 +10,11 @@ function getSafariExtensionEntitlements(iosConfig) {
         ],
     };
     const appattestKey = 'com.apple.developer.devicecheck.appattest-environment';
-    if ((_a = iosConfig === null || iosConfig === void 0 ? void 0 : iosConfig.entitlements) === null || _a === void 0 ? void 0 : _a[appattestKey]) {
+    const appgroupSecurity = 'com.apple.security.application-groups';
+    if ((_a = iosConfig === null || iosConfig === void 0 ? void 0 : iosConfig.entitlements) === null || _a === void 0 ? void 0 : _a[appgroupSecurity]) {
+        entitlements[appgroupSecurity] = iosConfig.entitlements[appgroupSecurity];
+    }
+    if ((_b = iosConfig === null || iosConfig === void 0 ? void 0 : iosConfig.entitlements) === null || _b === void 0 ? void 0 : _b[appattestKey]) {
         entitlements[appattestKey] = iosConfig.entitlements[appattestKey];
     }
     (iosConfig === null || iosConfig === void 0 ? void 0 : iosConfig.associatedDomains) &&
